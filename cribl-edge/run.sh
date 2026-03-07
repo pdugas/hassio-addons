53e8c7e9d3afc05804c22c9c7c501b2a34ecf2ad
Paul Dugas <pdugas@cribl.io>
2026-03-07 14:22:03 -0500

    fix ingress

--- cribl-edge/run.sh
+++ cribl-edge/run.sh
@@ -6,17 +6,23 @@ bashio::log.info "Dist-Mode: \"$(bashio::config 'CRIBL_DIST_MODE')\""
 export CRIBL_DIST_MODE=$(bashio::config 'CRIBL_DIST_MODE')
 export CRIBL_DIST_MASTER_URL=$(bashio::config 'CRIBL_DIST_MASTER_URL')
 
-bashio::log.info "Base URL: \"$(bashio::addon.ingress_entry)\""
+INGRESS_ENTRY="$(bashio::addon.ingress_entry)"
+bashio::log.info "Base URL: \"${INGRESS_ENTRY}\""
 CRIBL_YML=${CRIBL_VOLUME_DIR}/local/edge/cribl.yml
 if [ -e ${CRIBL_YML} ]; then
   if grep -q baseUrl ${CRIBL_YML}; then
-    sed -i "s|^  baseUrl:.*$|  baseUrl: $(bashio::addon.ingress_entry)|" ${CRIBL_YML}
+    sed -i "s|^  baseUrl:.*$|  baseUrl: ${INGRESS_ENTRY}|" ${CRIBL_YML}
   else
-    sed -i "s|^api:|api:\n  baseUrl: $(bashio::addon.ingress_entry)|" ${CRIBL_YML}
+    sed -i "s|^api:|api:\n  baseUrl: ${INGRESS_ENTRY}|" ${CRIBL_YML}
   fi
-else 
+  if ! grep -q proxyStripsPath ${CRIBL_YML}; then
+    sed -i "/^  baseUrl:/a\  proxyStripsPath: true" ${CRIBL_YML}
+  else
+    sed -i "s|^  proxyStripsPath:.*$|  proxyStripsPath: true|" ${CRIBL_YML}
+  fi
+else
   mkdir -p ${CRIBL_VOLUME_DIR}/local/edge
-  echo -e "api:\n  baseUrl: $(bashio::addon.ingress_entry)\n  host: 0.0.0.0\n  port: 9420" > ${CRIBL_YML}
+  echo -e "api:\n  baseUrl: ${INGRESS_ENTRY}\n  proxyStripsPath: true\n  host: 0.0.0.0\n  port: 9420" > ${CRIBL_YML}
 fi
 
 /opt/cribl/bin/cribl start
